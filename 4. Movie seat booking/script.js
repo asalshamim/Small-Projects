7bc782eb17712e35697cc94bf378125cbbc7d5ed
Asal <shamimasal8@gmail.com>
2021-12-13 21:58:34 +0100

    Ok

--- 4. Movie seat booking/script.js
+++ 4. Movie seat booking/script.js
@@ -6,12 +6,23 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
 
+// Save selected movie index and price
+
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+
 // Update total and count
 
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-    console.log(selectedSeats);
+     
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    
 
     const selectedSeatsCount = selectedSeats.length;
    
@@ -23,6 +34,7 @@ function updateSelectedCount() {
 
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value
+    setMovieData(e.target.seatsIndex, e.target.value);
     updateSelectedCount();
 });
 
