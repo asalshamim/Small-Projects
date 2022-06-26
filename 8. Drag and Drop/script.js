95ebc8bf0899dd268c8e634f1a51749a68ccb93f
Asal Shamim <41086746+asalshamim@users.noreply.github.com>
2022-06-26 12:24:16 +0200

    ok

--- 8. Drag and Drop/script.js
+++ 8. Drag and Drop/script.js
@@ -16,88 +16,104 @@ const richestPeople = [
 ];
 
 // Store listitems
-
-
 const listItems = [];
+
 let dragStartIndex;
 
 createList();
 
-const number = [1, 3, 110, 40, 302];
-console.log(number.sort(function (a, b) {
-    return a - b;
-}));
-
-// Insert List Items into Dom
-
+// Insert list items into DOM
 function createList() {
-    [...richestPeople]
-        .map(a => ({ value: a, sort: Math.random() }))
-        .sort((a, b) => a.sort - b.sort)
-        .map(a => a.value)
-        .forEach((person, index) => {
-           // console.log(person);
-            const listItem = document.createElement('li');
+  [...richestPeople]
+    .map(a => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(a => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement('li');
 
-           // listItem.classList.add('over');
-            listItem.setAttribute('data-index', index);
+      listItem.setAttribute('data-index', index);
 
-            listItem.innerHTML = `
-            <span class="number">${index + 1}</span>
-            <div class="draggable" draggable ="true">
-            <p class= "person-name">${person}</p>
-            <i class ="fas fa-grip-lines"></i>
-            </div>
-            `;
-            listItems.push(listItem);
-            draggable_list.appendChild(listItem);
-        });
-    
-    addEventListeners();
+      listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="person-name">${person}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+      `;
+
+      listItems.push(listItem);
+
+      draggable_list.appendChild(listItem);
+    });
+
+  addEventListeners();
 }
 
 function dragStart() {
-    //console.log('Event: ', 'dragstart');
-
-    dragStartIndex = +this.closest('li').getAttribute('data-index');
-    console.log(dragStartIndex);
+  // console.log('Event: ', 'dragstart');
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
 }
 
 function dragEnter() {
-   // console.log('Event: ', 'dragenter');
-    this.createList.add('over');
+  // console.log('Event: ', 'dragenter');
+  this.classList.add('over');
 }
 
 function dragLeave() {
-    //console.log('Event: ', 'dragleave');
-    this.createList.remove('over');
+  // console.log('Event: ', 'dragleave');
+  this.classList.remove('over');
 }
 
-function dragOver() {
-    //console.log('Event: ', 'dragover');
+function dragOver(e) {
+  // console.log('Event: ', 'dragover');
+  e.preventDefault();
 }
 
 function dragDrop() {
-   // console.log('Event: ', 'drop');
+  // console.log('Event: ', 'drop');
+  const dragEndIndex = +this.getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove('over');
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+// Check the order of list items
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector('.draggable').innerText.trim();
+
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add('wrong');
+    } else {
+      listItem.classList.remove('wrong');
+      listItem.classList.add('right');
+    }
+  });
 }
 
 function addEventListeners() {
-    const draggables = document.querySelectorAll('.draggable');
-    const dragListItems = document.querySelectorAll('.draggable-list li');
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItems = document.querySelectorAll('.draggable-list li');
 
-    draggables.forEach(draggable => {
-        draggable.addEventListener('dragstart', dragStart);
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', dragStart);
+  });
 
-    });
-
-    dragListItems.forEach(item => {
-        item.addEventListener('dragover', dragOver);
-        item.addEventListener('drop', dragDrop);
-        item.addEventListener('dragenter', dragEnter);
-        item.addEventListener('dragleave', dragLeave);
-        
-    });
-
-
-    
+  dragListItems.forEach(item => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
+  });
 }
+
+check.addEventListener('click', checkOrder);
